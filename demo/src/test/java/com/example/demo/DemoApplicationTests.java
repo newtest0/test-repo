7247907976cd94cfe7d5ec01aafa49aa81390338
Austin Zhang <austinyizhang@gmail.com>
2019-07-17 16:47:49 -0400

    project initial commit

---
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
+@AutoConfigureMockMvc
+public class DemoApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void helloGradle() throws Exception {
+		mvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string("Hello Gradle!"));
+	}
+
+}
